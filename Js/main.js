bee1035b9f3b813d7cbc8a61e3de9a44f0448eec
KailStrong <kailstrong@gmail.com>
2022-09-04 00:07:55 -0400

    first commit

---
+++ Js/main.js
@@ -0,0 +1,8 @@
+document.querySelector(".menu_btn").addEventListener("click", () =>{
+    document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+ScrollReveal().reveal('.ShowCase');
+ScrollReveal().reveal('.NewCards',{delay:500});
+ScrollReveal().reveal('.banner_one',{delay:500});
+ScrollReveal().reveal('.banner_two',{delay:500});
